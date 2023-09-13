55c249e84233ad6b7f1d2e87b3834eaa62b1aad1
Shounoop <hmtuan9a@gmail.com>
2023-09-13 12:53:51 +0700

    @SpringBootApplication annotation

--- src/main/java/com/amigoscode/Main.java
+++ src/main/java/com/amigoscode/Main.java
@@ -1,11 +1,17 @@
 package com.amigoscode;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+//@ComponentScan(basePackages = "com.amigoscode")
+//@EnableAutoConfiguration
+//@Configuration
 @RestController
 public class Main {
     public static void main(String[] args) {
