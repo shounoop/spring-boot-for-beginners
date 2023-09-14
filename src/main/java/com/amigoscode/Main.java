c1e489d93ad740232c375682da8532868be1bd6d
Shounoop <hmtuan9a@gmail.com>
2023-09-14 10:20:51 +0700

    read customers api

--- src/main/java/com/amigoscode/Main.java
+++ src/main/java/com/amigoscode/Main.java
@@ -3,27 +3,27 @@ package com.amigoscode;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("api/v1/customers")
 public class Main {
+    private final CustomerRepository customerRepository;
+
+    public Main(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
 
-    @GetMapping("/greet")
-    public GreetResponse greet() {
-        GreetResponse response = new GreetResponse("Hello", List.of("JS", "Java"), new Person("Shounoop", 22, 99_000));
-
-        return response;
-    }
-
-    record Person(String name, int age, double savings) {
-    }
-
-    record GreetResponse(String greet, List<String> favProgrammingLanguages, Person person) {
+    @GetMapping
+    public List<Customer> getCustomers() {
+        return this.customerRepository.findAll();
     }
 }
